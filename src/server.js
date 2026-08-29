b3e8b996e0e599e30b7ae57ebc74e4dcb2a6fb3e
Rolin Chavez <rolindev99@gmail.com>
2026-08-28 23:20:12 -0500

    cambios

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const trabajadores = [
         salario: 3000
     },
     {
-        id: 1,
+        id: 3,
         nombre: "Pedro",
         apellido: "Ramirez",
         cargo: "Administrador",
@@ -28,15 +28,37 @@ const trabajadores = [
     }
 ];
 
+/**
+ * Muestra un mensaje de Bienvenida.
+ */
 app.get('/', (req, res) => {
     res.json({
         mensaje: 'Bienvenido al Curso de Node.js con Express y Mysql.'
     });
 });
 
+/**
+ * Listar Trabajadores
+ */
 app.get('/trabajadores', (req, res) => {
     res.json(trabajadores);
 });
+
+/**
+ * Buscar Trabajador por ID
+ * :id = 2
+ * :id = 3
+ * const id = '2'
+ */
+
+app.get('/trabajadores/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const trabajador = trabajadores.find(trabajador => trabajador.id === id);
+    if(!trabajador) {
+       res.json({mensaje: "No encontrado"}) 
+    }
+    res.json(trabajador);
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecuntándose en http://localhost:${PORT}`);
