cddc09df75b5e21ae260e4cd9995bc715b8a6cd0
Rolin Chavez <rolindev99@gmail.com>
2026-08-30 12:35:58 -0500

    feat: crud node - practica, mysql

--- src/server.js
+++ src/server.js
@@ -1,65 +1,36 @@
 import express from 'express';
+import pool from './config/db.js';
 
 const app = express();
 
 const PORT = 3000;
 
-const trabajadores = [
-    {
-        id: 1,
-        nombre: "Juan",
-        apellido: "Perez",
-        cargo: "Programador",
-        salario: 2500
-    },
-    {
-        id: 2,
-        nombre: "Maria",
-        apellido: "Gomez",
-        cargo: "Analista",
-        salario: 3000
-    },
-    {
-        id: 3,
-        nombre: "Pedro",
-        apellido: "Ramirez",
-        cargo: "Administrador",
-        salario: 2800
-    }
-];
-
-/**
- * Muestra un mensaje de Bienvenida.
- */
 app.get('/', (req, res) => {
     res.json({
-        mensaje: 'Bienvenido al Curso de Node.js con Express y Mysql.'
-    });
+        mesnaje: 'Bienvenido al curso de Node.js con Express y MySQL'
+    })
 });
 
-/**
- * Listar Trabajadores
- */
-app.get('/trabajadores', (req, res) => {
-    res.json(trabajadores);
-});
+// async function iniciarServidor() {
+//     try {
+        
+//         await pool.query('SELECT 1');
 
-/**
- * Buscar Trabajador por ID
- * :id = 2
- * :id = 3
- * const id = '2'
- */
+//         console.log('Conexión a MySQL exitosa.');
 
-app.get('/trabajadores/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const trabajador = trabajadores.find(trabajador => trabajador.id === id);
-    if(!trabajador) {
-       res.json({mensaje: "No encontrado"}) 
-    }
-    res.json(trabajador);
-})
+//         app.listen(PORT, () => {
+//             console.log(`Servidor ejecuntándose en http://localhost:${PORT}`);
+//         })
+        
+
+//     } catch (error) {
+//         console.log(error); 
+//     }
+// }
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecuntándose en http://localhost:${PORT}`);
 })
+
+// iniciarServidor()
+
